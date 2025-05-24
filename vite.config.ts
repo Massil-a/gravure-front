6ac8be63ed30bf210f421cfa68e0e314890cda2e
Massil <amirat.massil@gmail.com>
2025-05-24 16:46:50 +0200

    Render6

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(__dirname, 'dist'), // Chemin absolu
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
 });
