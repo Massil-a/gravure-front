93e7a1c95af8b588278cbcb9320af7a243b6a293
Massil <amirat.massil@gmail.com>
2025-05-24 16:46:01 +0200

    Render5

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(__dirname, 'dist'), // Chemin absolu
     emptyOutDir: true,
   },
 });
